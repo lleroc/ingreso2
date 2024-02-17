2308314b5ac41806c44c16435880b84491798e6f
Lleroc <llleroc@yahoo.es>
2024-02-16 20:20:30 -0500

    accesos_model

--- models/accesos.models.php
+++ models/accesos.models.php
@@ -24,42 +24,16 @@ class Accesos
         $con->close();
     }
     /*TODO: Procedimiento para insertar */
-    public function Insertar($Ultimo, $Usuarios_idUsuarios, $tipo)
+    public function Insertar($Ultimo, $Usuarios_idUsuarios, $IdTipoAcceso)
     {
         $con = new ClaseConectar();
         $con = $con->ProcedimientoConectar();
-        $cadena = "INSERT into Accesos(Ultimo,Usuarios_idUsuarios,tipo) values ( '$Ultimo', $Usuarios_idUsuarios, '$tipo')";
+        $cadena = "INSERT into Accesos(Ultimo,Usuarios_idUsuarios,IdTipoAcceso) values ( '$Ultimo', $Usuarios_idUsuarios, '$IdTipoAcceso')";
 
         if (mysqli_query($con, $cadena)) {
             return "ok";
         } else {
             return mysqli_error($con);
-        }
-        $con->close();
-    }
-    /*TODO: Procedimiento para actualizar */
-    public function Actualizar($idAccesos, $Ultimo, $Usuarios_idUsuarios)
-    {
-        $con = new ClaseConectar();
-        $con = $con->ProcedimientoConectar();
-        $cadena = "update Accesos set Ultimo='$Ultimo',Usuarios_idUsuarios=$Usuarios_idUsuarios where idAccesos= $idAccesos";
-        if (mysqli_query($con, $cadena)) {
-            return "ok";
-        } else {
-            return 'error al actualizar el registro';
-        }
-        $con->close();
-    }
-    /*TODO: Procedimiento para Eliminar */
-    public function Eliminar($idAccesos)
-    {
-        $con = new ClaseConectar();
-        $con = $con->ProcedimientoConectar();
-        $cadena = "delete from Accesos where idAccesos = $idAccesos";
-        if (mysqli_query($con, $cadena)) {
-            return true;
-        } else {
-            return false;
         }
         $con->close();
     }
