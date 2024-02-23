9715c98ff3c25526e9a74957557a1427d4d50ca0
Lleroc <llleroc@yahoo.es>
2024-02-22 20:50:10 -0500

    BDD

--- docs/BDD.sql
+++ docs/BDD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 17-02-2024 a las 01:38:43
+-- Tiempo de generación: 23-02-2024 a las 01:49:46
 -- Versión del servidor: 5.7.39
 -- Versión de PHP: 8.2.0
 
@@ -24,9 +24,44 @@ SET time_zone = "+00:00";
 CREATE TABLE `Accesos` (
   `idAccesos` int(11) NOT NULL,
   `IdTipoAcceso` int(11) NOT NULL,
-  `Ultimo` datetime NOT NULL,
-  `Usuarios_idUsuarios` int(11) NOT NULL
+  `Ultimo` datetime DEFAULT NULL,
+  `Usuarios_idUsuarios` int(11) NOT NULL,
+  `imagen` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Accesos`
+--
+
+INSERT INTO `Accesos` (`idAccesos`, `IdTipoAcceso`, `Ultimo`, `Usuarios_idUsuarios`, `imagen`) VALUES
+(1, 2, NULL, 3, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Empleado`
+--
+
+CREATE TABLE `Empleado` (
+  `EmpleadoId` int(11) NOT NULL,
+  `Nombres` text NOT NULL,
+  `Apellidos` text NOT NULL,
+  `Direccion` text NOT NULL,
+  `Telefono` text NOT NULL,
+  `Cedula` text NOT NULL,
+  `Correo` text NOT NULL,
+  `RolId` int(11) NOT NULL,
+  `SucursalId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `Empleado`
+--
+
+INSERT INTO `Empleado` (`EmpleadoId`, `Nombres`, `Apellidos`, `Direccion`, `Telefono`, `Cedula`, `Correo`, `RolId`, `SucursalId`) VALUES
+(2, 'Luis Antonio', 'Llerena Ocaña', 'RAZO RAZO', '0981030167', '1234567890', 'lleroc1@gmail.com', 5, 5),
+(5, 'Luis Antonio', 'Llerena Ocaña', 'RAZO RAZO', '0981030167', 'lhiluh', 'kjbs@gmail.com', 5, 6),
+(6, 'Luis Antonio', 'Llerena Ocaña', 'RAZO RAZO', '0981030167', 'lhiluh', 'kjbs@gmail.com', 6, 6);
 
 -- --------------------------------------------------------
 
@@ -90,8 +125,7 @@ CREATE TABLE `Tipo_Acceso` (
 INSERT INTO `Tipo_Acceso` (`IdTipoAcceso`, `Detalle`) VALUES
 (1, 'Ingreso'),
 (2, 'Salida'),
-(3, 'Ingreso Almuerzo'),
-(4, 'Salida Almuerzo');
+(3, 'Ingreso Almuerzo');
 
 -- --------------------------------------------------------
 
@@ -106,19 +140,28 @@ CREATE TABLE `Usuarios` (
   `contrasenia` varchar(45) NOT NULL,
   `Correo` varchar(45) NOT NULL,
   `SucursalId` int(11) NOT NULL,
-  `Cedula` varchar(17) NOT NULL
+  `Cedula` varchar(17) NOT NULL,
+  `imagen` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `Usuarios`
 --
 
-INSERT INTO `Usuarios` (`idUsuarios`, `Nombres`, `Apellidos`, `contrasenia`, `Correo`, `SucursalId`, `Cedula`) VALUES
-(3, 'Luis', 'Llerena', '123', 'correo@gmail.com', 5, '1803971371'),
-(4, 'xxxx', 'xxxx', '123', 'otro@otro.com', 5, '1804233151'),
-(5, 'Luis Antonio', 'Llerena Ocaña', '123', 'lleroc@gmail.com', 6, '1801770569'),
-(6, 'kjhvkjhbk', 'bkjhbkjhbkjhb', '', '', 6, '1802491181'),
-(7, 'kjhvkjhbk', 'bkjhbkjhbkjhb', '', '', 6, '1803971730');
+INSERT INTO `Usuarios` (`idUsuarios`, `Nombres`, `Apellidos`, `contrasenia`, `Correo`, `SucursalId`, `Cedula`, `imagen`) VALUES
+(3, 'Luis', 'Llerena', '123', 'correo@gmail.com', 5, '1803971371', ''),
+(4, 'xxxx', 'xxxx', '123', 'otro@otro.com', 5, '1804233151', ''),
+(5, 'Luis Antonio', 'Llerena Ocaña', '123', 'lleroc@gmail.com', 6, '1801770569', ''),
+(6, 'kjhvkjhbk', 'bkjhbkjhbkjhb', '', '', 6, '1802491181', ''),
+(7, 'kjhvkjhbk', 'bkjhbkjhbkjhb', '', '', 6, '1803971730', ''),
+(8, 'Luis', 'Llerena', '123', 'lleroc@gmail.com', 5, '1234567890', '../8.jpeg'),
+(9, 'Luis Antonio', 'Llerena Ocaña', '123', 'otro@otro.com', 6, '1801704279', '../9.jpeg'),
+(10, 'Juan', 'Pujar', '123', 'otro@otro.com', 6, '1234567890', '../../public/images/usuarios/10.jpeg'),
+(11, 'Luis', 'Llerena', '123', 'lleroc@gmail.com', 5, '1234567890', '../../public/images/usuarios/11.jpeg'),
+(12, 'xxxx', 'xxxx', '123', 'otro@otro.com', 5, '1804233151', '../../public/images/usuarios/12.jpeg'),
+(13, 'Jessenia', 'Flores', '123', 'lleroc@gmail.com', 5, '1234567890', '../../public/images/usuarios/13.jpeg'),
+(14, 'Juan', 'xxxx', '123', 'otro@otro.com', 5, '1234567890', '../../public/images/usuarios/14.pdf'),
+(16, 'xxxx', 'Llerena Ocaña', '123', 'crr@gmso.com', 6, '1803971372', '../../public/images/usuarios/16.jpeg');
 
 -- --------------------------------------------------------
 
@@ -137,10 +180,10 @@ CREATE TABLE `Usuarios_Roles` (
 --
 
 INSERT INTO `Usuarios_Roles` (`Usuarios_idUsuarios`, `Roles_idRoles`, `idUsuariosRoles`) VALUES
-(3, 5, 3),
 (4, 5, 4),
-(5, 6, 5),
-(7, 6, 7);
+(8, 5, 8),
+(11, 5, 11),
+(16, 5, 15);
 
 --
 -- Índices para tablas volcadas
@@ -153,6 +196,14 @@ ALTER TABLE `Accesos`
   ADD PRIMARY KEY (`idAccesos`),
   ADD KEY `fk_Accesos_Usuarios1_idx` (`Usuarios_idUsuarios`),
   ADD KEY `Acceso_tipoAcceso` (`IdTipoAcceso`);
+
+--
+-- Indices de la tabla `Empleado`
+--
+ALTER TABLE `Empleado`
+  ADD PRIMARY KEY (`EmpleadoId`),
+  ADD KEY `empleado_sucursal` (`SucursalId`),
+  ADD KEY `empleado_rol` (`RolId`);
 
 --
 -- Indices de la tabla `Roles`
@@ -195,7 +246,13 @@ ALTER TABLE `Usuarios_Roles`
 -- AUTO_INCREMENT de la tabla `Accesos`
 --
 ALTER TABLE `Accesos`
-  MODIFY `idAccesos` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idAccesos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `Empleado`
+--
+ALTER TABLE `Empleado`
+  MODIFY `EmpleadoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `Roles`
@@ -207,25 +264,25 @@ ALTER TABLE `Roles`
 -- AUTO_INCREMENT de la tabla `Sucursales`
 --
 ALTER TABLE `Sucursales`
-  MODIFY `SucursalId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `SucursalId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `Tipo_Acceso`
 --
 ALTER TABLE `Tipo_Acceso`
-  MODIFY `IdTipoAcceso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `IdTipoAcceso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `Usuarios`
 --
 ALTER TABLE `Usuarios`
-  MODIFY `idUsuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUsuarios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de la tabla `Usuarios_Roles`
 --
 ALTER TABLE `Usuarios_Roles`
-  MODIFY `idUsuariosRoles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUsuariosRoles` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
@@ -239,6 +296,13 @@ ALTER TABLE `Accesos`
   ADD CONSTRAINT `fk_Accesos_Usuarios1` FOREIGN KEY (`Usuarios_idUsuarios`) REFERENCES `Usuarios` (`idUsuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Filtros para la tabla `Empleado`
+--
+ALTER TABLE `Empleado`
+  ADD CONSTRAINT `empleado_rol` FOREIGN KEY (`RolId`) REFERENCES `Roles` (`idRoles`),
+  ADD CONSTRAINT `empleado_sucursal` FOREIGN KEY (`SucursalId`) REFERENCES `Sucursales` (`SucursalId`);
+
+--
 -- Filtros para la tabla `Usuarios`
 --
 ALTER TABLE `Usuarios`
@@ -249,5 +313,5 @@ ALTER TABLE `Usuarios`
 --
 ALTER TABLE `Usuarios_Roles`
   ADD CONSTRAINT `fk_Usuarios_has_Roles_Roles1` FOREIGN KEY (`Roles_idRoles`) REFERENCES `Roles` (`idRoles`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Usuarios_has_Roles_Usuarios1` FOREIGN KEY (`Usuarios_idUsuarios`) REFERENCES `Usuarios` (`idUsuarios`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Usuarios_has_Roles_Usuarios1` FOREIGN KEY (`Usuarios_idUsuarios`) REFERENCES `Usuarios` (`idUsuarios`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
